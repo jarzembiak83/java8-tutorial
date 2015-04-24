64e3b71d6fe511340edd8a16fb7e3f5b4c3746d1
Marcin Jarzembowski <marcin.jarzembowski@allegrogroup.com>
2015-04-24 08:59:23 +0200

    Lambdas - last part of tutorial
    Change-Id: I7fced96643392841a0d64d3c2e49c22edb59cd60

--- src/com/mjlab/tutorial/java8/lambdas/common/FunctionUtils.java
+++ src/com/mjlab/tutorial/java8/lambdas/common/FunctionUtils.java
@@ -18,7 +18,21 @@ public class FunctionUtils {
         return result;
     }
 
+    private static <T> Predicate<T> anyPassPredicate(Predicate<T>... p) {
+        Predicate<T> result = t -> false;
+
+        for (Predicate<T> predicate : p) {
+            result = result.or(predicate);
+        }
+
+        return result;
+    }
+
     public static <T> T firstAllMatch(Stream<T> s, Predicate<T>... p) {
         return s.filter(allPassPredicate(p)).findFirst().get();
+    }
+
+    public static <T> T firstAnyMatch(Stream<T> s, Predicate<T>... p) {
+        return s.filter(anyPassPredicate(p)).findFirst().get();
     }
 }
--- src/com/mjlab/tutorial/java8/lambdas/part3/LambdaExcercisesPart3.java
+++ src/com/mjlab/tutorial/java8/lambdas/part3/LambdaExcercisesPart3.java
@@ -1,8 +1,6 @@
 package com.mjlab.tutorial.java8.lambdas.part3;
 
-import com.mjlab.tutorial.java8.lambdas.common.ElementUtils;
 import com.mjlab.tutorial.java8.lambdas.common.FunctionUtils;
-import com.mjlab.tutorial.java8.lambdas.common.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,13 +10,20 @@ import java.util.List;
  */
 public class LambdaExcercisesPart3 {
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("assosssss", "btoyyy", "rty", "12ty", "edddd", "acvvv", "oooo");
+        List<String> words = Arrays.asList("o", "assosssss", "btoyyy", "rty", "12ty", "edddd", "acvvv", "oooo");
         System.out.println("Initial strings array");
         System.out.println(words);
 
-        String firstMatch = FunctionUtils.firstAllMatch(words.stream(),
+        String firstAllMatch = FunctionUtils.firstAllMatch(words.stream(),
                 s -> s.contains("o"),
                 s -> s.length() > 5);
-        System.out.println(firstMatch);
+        System.out.println("firstAllMatch");
+        System.out.println(firstAllMatch);
+
+        String firstAnyMatch =  FunctionUtils.firstAnyMatch(words.stream(),
+                s -> s.contains("o"),
+                s -> s.length() > 5);
+        System.out.println("firstAnyMatch");
+        System.out.println(firstAnyMatch);
     }
 }
